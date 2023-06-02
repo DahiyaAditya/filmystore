dd3da667cdf7734d44b688afc17e9adaa4786fac
aditya <aditya223307@gmail.com>
2023-06-02 15:16:22 +0530

    Detail page is done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Card from './components/Card';
 import AddMovie from './components/AddMovie';
 import {  Route, Routes} from 'react-router-dom';
-
+import Details from './components/Details';
 function App() {
   return (
    <>
@@ -11,6 +11,7 @@ function App() {
    <Routes>
       <Route path='/' element={<Card></Card>}></Route>
       <Route path='/addMovie' element={<AddMovie></AddMovie>}></Route>
+      <Route path='/detail/:id' element={<Details></Details>}></Route>
    </Routes>
    </>
     
