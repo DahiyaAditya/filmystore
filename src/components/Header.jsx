3e0463a7b24319543f452601bea1364e725a2986
aditya <aditya223307@gmail.com>
2023-06-04 17:18:51 +0530

    done with login and signup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import AddIcon from '@mui/icons-material/Add';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
-
+import { appState } from '../App';
 const Header = () => {
+  const useAppState = useContext(appState)
   return (
    <>
   <div className='header'>
        <Link to={'/'}> <h1 className='main-name'>Filmy<span className='name-part2'>Store</span></h1></Link>
        <div className="fav">
-        <Link to={'/addMovie'}>
-           <Button  className='button-85'><AddIcon></AddIcon>Add Your Favorite</Button> 
-        </Link>
+       {useAppState.login ?
+         <Link to={'/addMovie'}>
+         <Button  className='button-85'><AddIcon></AddIcon>Add Your Favorite</Button> 
+      </Link>
+      :
+      <Link to={'/login'}>
+         <Button  className='button-85' style={{color:"red"}}>Login</Button> 
+      </Link>
+      }
+
+       
        </div>
   </div>
    </>
